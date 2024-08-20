b1d5bad85fa1cc335c155062a4d3f87e5ee9c3bf
mbaaskth <kth@mbaas.kr>
2024-08-20 15:13:18 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,34 @@ export default function Home() {
   const [message, setMessage] = useState("");
   const [pendingRequests, setPendingRequests] = useState({});
 
+    const popupCenter = (url, title) => {
+    const dualScreenLeft = window.screenLeft ?? window.screenX;
+    const dualScreenTop = window.screenTop ?? window.screenY;
+
+    const width =
+      window.innerWidth ?? document.documentElement.clientWidth ?? screen.width;
+
+    const height =
+      window.innerHeight ??
+      document.documentElement.clientHeight ??
+      screen.height;
+
+    const systemZoom = width / window.screen.availWidth;
+
+    const left = (width - 500) / 2 / systemZoom + dualScreenLeft;
+    const top = (height - 550) / 2 / systemZoom + dualScreenTop;
+
+    const newWindow = window.open(
+      url,
+      title,
+      `width=${500 / systemZoom},height=${550 / systemZoom
+      },top=${top},left=${left}`
+    );
+
+    newWindow?.focus();
+    };
+
+
   // 플러터에 메시지를 보내는 함수
   const sendFlutterRequest = (action, data) => {
     const requestId = uuidv4(); // 고유한 requestId 생성
